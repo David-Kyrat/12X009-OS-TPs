8ff79ab42f0fe59764a82479ab9f6fea7e0fdad3
David Kyrat <33601938+David-Kyrat@users.noreply.github.com>
2022-09-27 15:42:33 +0200

    removed tmp script to create repo

--- tmp.ps1
+++
@@ -1,7 +0,0 @@
-echo "# 12X009-OS-TPs" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M master
-git remote add origin git@github.com:David-Kyrat/12X009-OS-TPs.git
-git push -u origin master
