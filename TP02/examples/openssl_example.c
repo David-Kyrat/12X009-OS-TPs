efa0db06d04b7cb1afd8202ce7d6fcda59968145
David Kyrat <33601938+David-Kyrat@users.noreply.github.com>
2022-10-11 16:50:12 +0200

    Added openssl example with string to hash

--- TP02/examples/openssl_example.c
+++ TP02/examples/openssl_example.c
@@ -5,8 +5,9 @@
 int main(int argc, char* argv[]) {
     EVP_MD_CTX* mdctx;
     const EVP_MD* md;
-    char mess1[] = "Test Message\n";
-    char mess2[] = "Hello World\n";
+    //char mess1[] = "Test Message\n";
+    //char mess2[] = "Hello World\n";
+    char test_mess[] = "Le manuel disait: Nécessite Windows 7 ou mieux. J'ai donc installé Linux\n";
     unsigned char md_value[EVP_MAX_MD_SIZE];
     unsigned int md_len, i;
 
@@ -24,14 +25,14 @@ int main(int argc, char* argv[]) {
 
     mdctx = EVP_MD_CTX_new();
     EVP_DigestInit_ex(mdctx, md, NULL);
-    EVP_DigestUpdate(mdctx, mess1, strlen(mess1));
-    EVP_DigestUpdate(mdctx, mess2, strlen(mess2));
+    EVP_DigestUpdate(mdctx, test_mess, strlen(test_mess));
+    /*EVP_DigestUpdate(mdctx, mess1, strlen(mess1));
+    EVP_DigestUpdate(mdctx, mess2, strlen(mess2));*/
     EVP_DigestFinal_ex(mdctx, md_value, &md_len);
     EVP_MD_CTX_free(mdctx);
 
-    printf("Digest is: ");
-    for (i = 0; i < md_len; i++)
-        printf("%02x", md_value[i]);
-    printf("\n");
+    printf("Digest is: \"");
+    for (i = 0; i < md_len; i++) printf("%02x", md_value[i]);
+    printf("\"\n");
     exit(0);
     }
