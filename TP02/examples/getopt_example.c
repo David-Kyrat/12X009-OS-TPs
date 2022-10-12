71a672f1833a18d45d22ae495a8f8c7f3bc9d809
David Kyrat <33601938+David-Kyrat@users.noreply.github.com>
2022-10-12 14:32:46 +0200

    modified getopt_example

--- TP02/examples/getopt_example.c
+++ TP02/examples/getopt_example.c
@@ -1,6 +1,7 @@
-#include <unistd.h>
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <getopt.h>
 
 int main(int argc, char* argv[]) {
     int flags, opt;
@@ -19,20 +20,19 @@ int main(int argc, char* argv[]) {
                 break;
             default: /* '?' */
                 fprintf(stderr, "Usage: %s [-t nsecs] [-n] name\n",
-                    argv[0]);
+                        argv[0]);
                 exit(EXIT_FAILURE);
-            }
         }
+    }
 
     printf("flags=%d; tfnd=%d; nsecs=%d; optind=%d\n",
-        flags, tfnd, nsecs, optind);
+           flags, tfnd, nsecs, optind);
 
     if (optind >= argc) {
         fprintf(stderr, "Expected argument after options\n");
         exit(EXIT_FAILURE);
 
         printf("name argument = %s\n", argv[optind]);
-        /* Other code omitted *
-        exit(EXIT_SUCCESS); */
-        }
+        exit(EXIT_SUCCESS);
     }
+}
