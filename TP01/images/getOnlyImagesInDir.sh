69b33b665634863ef1434e353d78522d69651a84
David Kyrat <33601938+David-Kyrat@users.noreply.github.com>
2022-10-12 15:03:03 +0200

    sorry jai deconne

--- TP01/images/getOnlyImagesInDir.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash 
-file -i * | cut -d ';' -f 1 | grep image | cut -d ':' -f 1 | sed -z 's/\n/,\n/g' > test.csv
- #last call to be able to treat it like csv
- while IFS="," read -r im; do echo $(tr -d "[' ''\"]" <<< $im); done < ./test.csv
