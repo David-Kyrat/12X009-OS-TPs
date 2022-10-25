2c49780ad7a094ceee36dceeea7b7d86a0f814d8
David Kyrat <33601938+David-Kyrat@users.noreply.github.com>
2022-10-25 13:52:38 +0200

    added error handling functions in util.c

--- TP03/util.c
+++ TP03/util.c
@@ -1,9 +1,7 @@
 /**
  * @file util.c
  * @brief Utility functions mostly used for error handling
- * 
  */
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <errno.h>
@@ -15,4 +13,52 @@ void* tryalc(void* allocReturn) {
     //*malloc returned null => exiting with error message ENOMEM
     fprintf(stderr, "%s, Cannot allocate Memory\n", strerror(errno));
     exit(ENOMEM);
+}
+
+
+int hdlOpenErr(char* filename, int needsExit) {
+    int savedErr = errno;
+    fprintf(stderr, "Could not open file %s : %s\n", filename, strerror(savedErr));
+    if (needsExit) exit(savedErr);
+    return -1;
+}
+
+int hdlCloseErr(char* filename, int needsExit) {
+    int savedErr = errno;
+    fprintf(stderr, "Could not close file %s : %s\n", filename, strerror(savedErr));
+    if (needsExit) exit(savedErr);
+    return -1;
+}
+
+int hdlReadErr(char* filename, int needsExit, int needsClose, int fd) {
+    int savedErr = errno;
+    fprintf(stderr, "Could not read from file %s : %s\n", filename, strerror(savedErr));
+    if (needsClose) {
+        if (close(fd)) hdlCloseErr(filename, needsExit);
+    }
+    
+    if (needsExit) exit(savedErr);
+    return -1;
+}
+
+int hdlWriteErr(char* filename, int needsExit, int needsClose, int fd) {
+    int savedErr = errno;
+    fprintf(stderr, "Could not write to file %s : %s\n", filename, strerror(savedErr));
+    if (needsClose) {
+        if (close(fd)) hdlCloseErr(filename, needsExit);
+    }
+    if (needsExit) exit(savedErr);
+    return -1;
+}
+
+int hdlCopyErr(char* from, char* to, int needsExit, int needsClose, int fd_from, int fd_to) {
+    int savedErr = errno;
+    fprintf(stderr, "Could not copy  %s to %s: %s\n", from, to, strerror(savedErr));
+    if (needsClose) {
+        if (close(fd_from)) hdlCloseErr(from, needsExit);
+        if (close(fd_to)) hdlCloseErr(to, needsExit);
+    }
+
+    if (needsExit) exit(savedErr);
+    return -1;
 }
--- TP03/util.h
+++ TP03/util.h
@@ -1,4 +1,8 @@
-
+/**
+ * @file util.h
+ * @brief Utility functions mostly used for error handling
+ * 
+ */
 
 /**
  * Receives the output of the "malloc attempt". If it's null, print an error message to stderr and exit
@@ -6,3 +10,69 @@
  * @return allocReturn if it's not null, exit otherwise
  */
 void* tryalc(void* allocReturn);
+
+/**
+ * Utility function for basic handling of error related to file openning.
+ * 
+ * Prints an error message to stderr and exits with the error code if needsExit is true, otherwise
+ * ust returns -1
+ * @param filename Name of the file that could not be opened.
+ * @param needsExit int, If non-zero, program will exit with the saved error code from errno. (otherwise do nothing)
+ * @return -1 or, exits with saved errno, if needsExit was != 0.
+ */
+int hdlOpenErr(char* filename, int exit);
+
+/**
+ * Utility function for basic handling of error related to file closing.
+ * 
+ * Prints an error message to stderr and exits with the error code if needsExit is true, otherwise
+ * ust returns -1
+ * @param filename Name of the file that could not be closed.
+ * @param needsExit int, If non-zero, program will exit with the saved error code from errno. (otherwise do nothing)
+ * @return -1 or, exits with saved errno, if needsExit was != 0.
+ */
+int hdlCloseErr(char* filename, int exit);
+
+/**
+ * Utility function for basic handling of error related to file reading.
+ * Prints an error message to stderr, closes the file descriptor if needed, and exits if needed
+ * 
+ * @param filename Name of the file that was being read from
+ * @param needsExit If non-zero, the program will exit with the error code of the error that
+ * occurred.
+ * @param needsClose if non-zero, function will attempt to close the given fd (also handles closing error if necessary)
+ * @param fd int, file descriptor of file being read from (can be null/0 if needsClose is 0)
+ * @return -1 or, exits with saved errno, if needsExit was != 0.
+ */
+int hdlReadErr(char* filename, int needsExit, int needsClose, int fd);
+
+/**
+ * Utility function for basic handling of error related to file writing.
+ * Prints an error message to stderr, closes the file descriptor if needed, and exits if needed
+ * 
+ * @param filename Name of the file that was being read from
+ * @param needsExit If non-zero, the program will exit with the error code of the error that
+ * occurred.
+ * @param needsClose if non-zero, function will attempt to close the given fd (also handles closing error if necessary)
+ * @param fd int, file descriptor of file being written to (can be null/0 if needsClose is 0)
+ * @return -1 or, exits with saved errno, if needsExit was != 0.
+ */
+int hdlWriteErr(char* filename, int needsExit, int needsClose, int fd);
+
+/**
+ * Utility function for basic handling of error related to file copying.
+ * 
+ * Prints an error message to stderr, close both given file descriptors if needsClose is non-zero 
+ * and exits with the error code if needsExit is true, otherwise just returns -1.
+ * Note: Also handles closing error if necessary
+ * 
+ * @param from Name of the file being copied
+ * @param to Name of the file being copied to
+ * @param needsExit int, If non-zero, program will exit with the saved error code from errno. (otherwise do nothing)
+ * @param needsClose int, If non-zero, function will close both file descriptors given as argument
+ * @param fd_from int, file descriptor of from (can be null/0 if needsClose is 0)
+ * @param fd_from int, file descriptor of to (can be null/0 if needsClose is 0)
+ * 
+ * @return -1 or, exits with saved errno, if needsExit was != 0.
+ */
+int hdlCopyErr(char* from, char* to, int needsExit, int needsClose, int fd_from, int fd_to);
