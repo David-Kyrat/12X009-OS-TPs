6e88f53186714effc9a16c91a89b22ccb9537825
David Kyrat <33601938+David-Kyrat@users.noreply.github.com>
2022-11-02 12:38:32 +0100

    added very basic prompt user input

--- TP04/main.c
+++ TP04/main.c
@@ -22,6 +22,18 @@
  * -- 5. whence (1 char) 's’ (SEEK_SET , default), ’c’ (SEEK_CUR), or ’e’ (SEEK_END)
  */
 
+int askInput(char* cmd, char* ltype, int* start, int* stop, char* whence) {
+    printf("enter cmd ltype start stop whence\n");
+    errno = 0;
+    scanf("%c %c %d %d %c", cmd, ltype, start, stop, whence);
+    if (errno != 0) {
+        int savedErr = errno;
+        fprintf(stderr, "Error when waiting for user input: %s\n", strerror(savedErr));
+        return -1;
+    }
+    return EXIT_SUCCESS;
+}
+
 int main(int argc, char* argv[]) {
 
     const char* file = parseArgs(argc, argv);
@@ -33,6 +45,15 @@ int main(int argc, char* argv[]) {
     }
 
     printf("file is %s\n", file);
+
+    char cmd, ltype, whence;
+    int start = 0, stop = 5;
+
+    if (askInput(&cmd, &ltype, &start, &stop, &whence) < 0) return -1;
+
+    printf("received input: \n");
+    printf("%c %c %d %d %c\n", cmd, ltype, start, stop, whence);
+    
 
     return EXIT_SUCCESS;
 }
