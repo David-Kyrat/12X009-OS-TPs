5e3d936991838423122a83eb307c93cd83cdd964
David Kyrat <33601938+David-Kyrat@users.noreply.github.com>
2022-11-26 15:48:05 +0100

    created cleaned TP06 branch

--- TP02/hash_calc.h
+++
@@ -1,5 +0,0 @@
-// Hashing function, which takes a string and a hash function as arguments, and will return a string, the hash digest
-unsigned char* hash(char* text, char* hash_f);
-
-// Function to convert a file's data into a string, which can be used as input for the hash function a file is inputted and a string is returned
-char* convert_f_to_s(char* filename);
