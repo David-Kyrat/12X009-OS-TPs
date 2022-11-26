3b15b37aaaf92385b69819392681f4044c293617
David Kyrat <33601938+David-Kyrat@users.noreply.github.com>
2022-11-26 15:40:47 +0100

    removed useless script

--- TP05/tmp.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/zsh
-git checkout c1c10ebc39fb9ed59bde7435b1a96fe89ba4b3e7
-cd ..
-cp TP05 -r ../ 
-git checkout master
-cd TP05 
-cp ../../TP05/* . -rf
-git commit -a -m "saving rendu-version of greg before doing anything"
-git push 
