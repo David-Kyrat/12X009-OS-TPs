05ed30879e9aa66076d97efa2329e2867e159d10
David Kyrat <33601938+David-Kyrat@users.noreply.github.com>
2022-12-06 23:28:14 +0100

    using the same sigaction struct makes everything bug

--- TP06/shell.c
+++ TP06/shell.c
@@ -399,7 +399,7 @@ int initSigHandlers(Shell* sh) {
     struct sigaction sa_ign; //use same sa make everything bug
     sa_ign.sa_handler = SIG_IGN;
     sa_ign.sa_sigaction = NULL;
-    sa_ign.flag = 0;
+    sa_ign.sa_flags = 0;
     for (int i = 0; i < IGNORE_NB; i++) {
         if (sigaction(SIG_TO_IGNORE[i], &sa, NULL) == -1) {
             const char msg[15];
