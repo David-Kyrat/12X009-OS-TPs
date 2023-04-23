6ac692802e983d79318a51d0dd308e443d3924ef
David Kyrat <33601938+David-Kyrat@users.noreply.github.com>
2023-04-23 22:38:23 +0200

    meaningful message

--- TP05/util.c
+++ TP05/util.c
@@ -4,10 +4,10 @@
  */
 #include <errno.h>
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
 #include <sys/stat.h>
 #include <sys/types.h>
+#include <stdlib.h>
 #include <time.h>
 #include <unistd.h>
 
